6eb96f987b73673965835f10c1f992e605f1dbde
Liu <liuhowa@czpcggspri14488.local>
2018-08-09 10:53:12 +0200

    Added more mode modules eslint and bodyparser

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require ('express');
 const app = express();
-port = process.env.PORT || 8080;
+const bodyParser = require('body-parser');
 
+port = process.env.PORT || 8080;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 app.listen(port);
 
 console.log('Server started on: ' + port);
