8c36b2d3d53fdd99780caccd33c4b6d4bbabe9e1
liuhowa <howard.liu@merck.com>
2018-08-15 15:32:01 +0200

    Added and tested database functions for CRUD

--- api/database/db_functions.js
+++ api/database/db_functions.js
@@ -1,8 +1,34 @@
-exports.addEntry = async (table, object)=>{
-    query = `INSERT INTO ${table} `;
+function addEntry(table, object){
+    let query = `INSERT INTO ${table} `;
     query = addObjectKeys(query, object);
     query += ` VALUES `;
     query = addObjectValues(query, object);
+
+    return query;
+}
+
+function getEntry(table, id) {
+    return query = `SELECT * FROM ${table} WHERE id=${id}`;
+}
+
+function deleteEntry(table, id){
+    return query = `DELETE FROM ${table} WHERE id=${id}`;
+}
+
+function updateEntry(table, object){
+    query = `UPDATE ${table} SET `;
+    query = equateObjectKeyValue(query, object);
+    query += ` WHERE id=${object.id}`;
+
+    console.log('query: ', query)
+    return query;
+}
+
+function equateObjectKeyValue(query, object){
+    for (let key in object){
+        query = query.concat(`${key} = '${object[key]}', `)
+    }
+    query = query.slice(0,-2);
 
     return query;
 }
@@ -27,3 +53,9 @@ function addObjectValues(query, object){
     return query;
 }
 
+module.exports = {
+    addEntry,
+    getEntry,
+    deleteEntry,
+    updateEntry
+}
