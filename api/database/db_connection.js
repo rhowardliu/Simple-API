c82098b2f683f892b55b81fc1fcd8bb537a30e51
liuhowa <howard.liu@merck.com>
2018-08-17 11:35:13 +0200

    Minor change in the way db_connection file is returned

--- api/database/db_connection.js
+++ api/database/db_connection.js
@@ -1,18 +1,22 @@
 const mysql = require('mysql');
-const Promise = require('promise');
 const db_config = require('./db_config')
 
 
 // let con =Promise.resolve(mysql.createConnection(db_config))
 // let con = mysql.createConnection(db_config);
 
-module.exports = ()=>{
-    let con = mysql.createConnection(db_config)
+let con = mysql.createConnection(db_config);
+
+
+dbConnect = ()=>{
     con.connect((err, result)=>{
         if (err) throw err;
         console.log ("Connected to MySQL database.");
         console.log ("result: ", result);
-
     });
-    return con;
+}
+
+module.exports = {
+    dbConnect,
+    con,
 }
